19e3241124e4e4e89539581e9ff1a0427a1d4280
william <alexander171296@gmail.com>
2019-07-10 19:48:16 -0400

    ajustando mas iconos

---
+++ js/estilos.js
@@ -0,0 +1,10 @@
+var placeholder1=document.getElementById('inputAddress2');
+var d;
+d='dsa';
+d.style.color='red';
+
+alert('b');
+placeholder1.value='adsf';
+for (var i = 0; i < placeholder1.length; i++) {
+	placeholder1.value='adsf';
+}
